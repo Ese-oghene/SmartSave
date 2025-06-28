e282f4b0481ffe5b61d8dc8c654f09d1d0bd574f
Ese-oghene <oghenee08@gmail.com>
2025-06-28 12:35:24 +0100

    feat: added new feature

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,11 +5,13 @@ import { HeroComponent } from './hero/hero.component';
 import { AboutComponent } from './about/about.component';
 import { FeaturesComponent } from './features/features.component';
 import { TestimonialsComponent } from './testimonials/testimonials.component';
+import { HowItWorksComponent } from './how-it-works/how-it-works.component';
+HowItWorksComponent
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, HeroComponent, AboutComponent, FeaturesComponent, TestimonialsComponent],
+  imports: [RouterOutlet, HeaderComponent, HeroComponent, HowItWorksComponent, AboutComponent, FeaturesComponent, TestimonialsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
