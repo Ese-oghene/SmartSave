fe644c831071f98e6eeb5448242c5ba7f8f85440
Ese-oghene <oghenee08@gmail.com>
2025-06-29 04:19:23 +0100

    feat: add login feature

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,14 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './header/header.component';
 import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+
 
 
 
 export const routes: Routes = [
  { path: '', component: HomeComponent},
  {path: 'header', component: HeaderComponent},
- { path: 'register', component: RegisterComponent }
-
+ { path: 'register', component: RegisterComponent },
+   { path: 'login', component: LoginComponent }
 ];
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,12 +1,21 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
 
+   email = '';
+  password = '';
+
+  login() {
+    console.log('Logging in with:', this.email, this.password);
+    // Add actual login logic here
+  }
 }
